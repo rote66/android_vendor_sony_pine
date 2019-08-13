24c0fde59edb29b437dd234b50861e3a952311a0
rote66 <pontifex23872388@gmail.com>
2019-08-13 22:34:27 +0800

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# android_vendor_sony_pine
-L1 G3311
+Vendor Repo for the pine
+=======================================
+
+Contains blobs (binaries) specific for sony pine
+These blobs correspond to the proprietary-files.txt file in the device tree (not yet)
---
+++ pine-vendor-blobs.mk
@@ -0,0 +1,9 @@
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/pine/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/pine/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/pine/vendor,system/vendor)
+
+
+
