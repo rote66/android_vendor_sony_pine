f1062f04eed0f6951f961fbd962cc66a34b10a93
rote66 <pontifex23872388@gmail.com>
2020-03-11 17:34:44 +0800

    color from source and some adjust

--- Android.mk
+++ Android.mk
@@ -56,16 +56,16 @@ LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudiocustparam.so
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE = libstagefright_color_conversion
-LOCAL_MODULE_CLASS = STATIC_LIBRARIES
-LOCAL_MODULE_SUFFIX = .a
-LOCAL_UNINSTALLABLE_MODULE = true
-LOCAL_MULTILIB = both
-LOCAL_SHARED_LIBRARIES += libdpframework
-LOCAL_SRC_FILES_64  := libstagefright_color_conversion/libstagefright_color_conversion_64.a
-LOCAL_SRC_FILES_32 := libstagefright_color_conversion/libstagefright_color_conversion_32.a
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE = libstagefright_color_conversion
+#LOCAL_MODULE_CLASS = STATIC_LIBRARIES
+#LOCAL_MODULE_SUFFIX = .a
+#LOCAL_UNINSTALLABLE_MODULE = true
+#LOCAL_MULTILIB = both
+#LOCAL_SHARED_LIBRARIES += libdpframework
+#LOCAL_SRC_FILES_64  := libstagefright_color_conversion/libstagefright_color_conversion_64.a
+#LOCAL_SRC_FILES_32 := libstagefright_color_conversion/libstagefright_color_conversion_32.a
+#include $(BUILD_PREBUILT)
 
 #include $(CLEAR_VARS)
 #LOCAL_MODULE := libmtkplayer
